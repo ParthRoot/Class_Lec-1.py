2f9a0892efb088bacbc146168cec95fd8026f8d9
Parth Patel <52830988+ParthRoot@users.noreply.github.com>
2020-05-07 16:09:37 +0530

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,16 @@
+#create class
+class test():
+    pass
+#create instace of class or Object
+parth = test()
+jaimin = test()
+
+#instance variable
+parth.name = "Parth"
+parth.MoNo = 963861317
+jaimin.name = "Jaimin"
+jaimin.MoNo = 9512449697
+
+#display instance variable's values
+print(parth.name,parth.MoNo)
+print(jaimin.name,jaimin.MoNo)
